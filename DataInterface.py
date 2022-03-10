cfd03b49f9d4e1c24d8fefef38c1704b90f48741
Herimino Rakotondrafara <heriminorakotondrafara@Heriminos-MacBook-Pro.local>
2022-03-10 11:57:14 -0500

    adjusted architecture

--- DataInterface.py
+++ DataInterface.py
@@ -6,15 +6,25 @@ import csv
 import pandas as pd
 import zlib
 
-def extractor():
-    myvars=[]
-    KeyValues=[]
+#ex of address :"data/lidarFile.txt"
+def extractor(address:str):
+    """Extracts .text file and returns an array of dictionaries
 
-    with open("data/lidarFile.txt") as myfile:
+    Returns:
+        _type_: list[dict[str:float]]
+    """
+    myvars=[]
+
+    with open(address) as myfile:
         for line in myfile:
             myvars.append(line)
+    return myvars
 
+def strings2dict(myvars:list[str]):
+    """converts a list of strings into a list of dictionary"""
+    KeyValues=[]
     for item in myvars:
+        #creates a list made of every line
         #theta: 1.09 Dist: 00616.00 Q: 47; change this into a dictionary
         words=item.split()
         if len(words)==6:#can improve this condition
@@ -23,13 +33,29 @@ def extractor():
                 str(words[2]).replace(":",""):float(words[3]), 
                 str(words[4]).replace(":",""):float(words[5])}
             KeyValues.append(dataPoint)
+    return KeyValues
 
-    #2 convert array for dictionaries to a dataframe
-    Cleaned_Data=pd.DataFrame(KeyValues)
+
+def convertDatatoPd(data:list[dict[str:float]]):
+    """converts a list of strings into a dataframe"""
+    Cleaned_Data=pd.DataFrame(data)
     return Cleaned_Data
 
+def sectionAtor(text:str):
+        """detects sectioning strings (for different scans)
+        
+        Args:
+            text (str): a string of text to be parsed.
 
+        Returns:
+            array[str]: an array of sectioned strings
+        """
+        #create a new array everytime we match the sectioning string
+        return
 
+"""
+
+"""
 def cleaner(data:pd.DataFrame):
     """formats extracted data:#step1: delete rows with Q!=47
 #step2: round up angles
@@ -62,15 +88,24 @@ def runPackager(displacement, run):
     return
     
 
-
-
 #IDEAS:
 """
 + returned numbers of spins are purely based on time
 +-> solution: we expect a data set for a fixed amount of time; start with a desired number of spins per unit distance (desired resolution)
 =>develop an interface which receives the speed to  adjust the number of spins recieved
+
+
+
+
+=>
 """
 
+def main():
+    data=cleaner(convertDatatoPd(strings2dict(extractor("data/lidarFile.txt"))))
+    return data
+    
+
+print(main())
 #subsampling before processing:
 #setup file accumulation interface to be able to compare different runs
 #https://towardsdatascience.com/how-to-automate-lidar-point-cloud-processing-with-python-a027454a536c
