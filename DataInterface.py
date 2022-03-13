9b0bfb3b6007bf01d21efcf5ca1978ceeeec59f5
Herimino Rakotondrafara <heriminorakotondrafara@Heriminos-MacBook-Pro.local>
2022-03-13 15:46:30 -0400

    working comparator

--- DataInterface.py
+++ DataInterface.py
@@ -3,71 +3,76 @@ import pandas as pd
 import numpy as np
 import math
 
-#ex of address :"data/lidarFile.txt"
-def extractor(address:str):
+# ex of address :"data/lidarFile.txt"
+
+
+def extractor(address: str):
     """Extracts .text file and returns an array of strings
 
     Returns:
         _type_: list[dict[str:float]]
     """
-    myvars=[]
+    myvars = []
 
     with open(address) as myfile:
         for line in myfile:
             myvars.append(line)
     return myvars
 
-def strings2dict(myvars:list[str]):
+
+def strings2dict(myvars: list[str]):
     """converts a list of strings into a list of dictionary"""
-    KeyValues=[]
+    KeyValues = []
     for item in myvars:
-        #creates a list made of every line
-        #theta: 1.09 Dist: 00616.00 Q: 47; change this into a dictionary
-        words=item.split()
-        if len(words)<=6:#can improve this condition
-            dataPoint={
-                str(words[0]).replace(":",""):float(words[1]),
-                str(words[2]).replace(":",""):float(words[3]), 
-                str(words[4]).replace(":",""):float(words[5])}
+        # creates a list made of every line
+        # theta: 1.09 Dist: 00616.00 Q: 47; change this into a dictionary
+        words = item.split()
+        if len(words) <= 6:  # can improve this condition
+            dataPoint = {
+                str(words[0]).replace(":", ""): float(words[1]),
+                str(words[2]).replace(":", ""): float(words[3]),
+                str(words[4]).replace(":", ""): float(words[5])}
             KeyValues.append(dataPoint)
     return KeyValues
 
 
-def convertDatatoPd(data:list[dict[str:float]]):
+def convertDatatoPd(data: list[dict[str:float]]):
     """converts a list of strings into a list of dataframe"""
-    Cleaned_Data=pd.DataFrame(data)
+    Cleaned_Data = pd.DataFrame(data)
     return Cleaned_Data
 
-def sectionAtor(data:list[str]):
-        """creates a list of list by detecting seperators
-        
-        Args:
-            text (str): a string of text to be parsed.
 
-        Returns:
-            array[str]: an array of sectioned strings
-        """
-        sublists:list=[]#to store newly accumulated sublist
-        temp_element:list[str]=[""]
-        currentSeparatorIndex=-1
-        previousSeparatorIndex=-1
-        for index,item in enumerate(data):
-            if len(item)<=8:
-                currentSeparatorIndex=index
-                temp_element=data[previousSeparatorIndex+1:currentSeparatorIndex]
-                if previousSeparatorIndex+1!=currentSeparatorIndex:
-                    sublists.append(temp_element)
-                previousSeparatorIndex=currentSeparatorIndex
-        #print(currentSeparatorIndex+1)
-        endElement=data[currentSeparatorIndex+1:]
-        if endElement!=[]:
-            sublists.append(endElement)
-        #sublists.append(data_copy)
-        #go through the list; when we meet a separator, append data from previous previous separator to current separator    
-        #create a new array everytime we match the sectioning string
-        return sublists
+def sectionAtor(data: list[str]):
+    """creates a list of list by detecting seperators
 
-def cleaner(data:pd.DataFrame):
+    Args:
+        text (str): a string of text to be parsed.
+
+    Returns:
+        array[str]: an array of sectioned strings
+    """
+    sublists: list = []  # to store newly accumulated sublist
+    temp_element: list[str] = [""]
+    currentSeparatorIndex = -1
+    previousSeparatorIndex = -1
+    for index, item in enumerate(data):
+        if len(item) <= 8:
+            currentSeparatorIndex = index
+            temp_element = data[previousSeparatorIndex+1:currentSeparatorIndex]
+            if previousSeparatorIndex+1 != currentSeparatorIndex:
+                sublists.append(temp_element)
+            previousSeparatorIndex = currentSeparatorIndex
+    # print(currentSeparatorIndex+1)
+    endElement = data[currentSeparatorIndex+1:]
+    if endElement != []:
+        sublists.append(endElement)
+    # sublists.append(data_copy)
+    # go through the list; when we meet a separator, append data from previous previous separator to current separator
+    # create a new array everytime we match the sectioning string
+    return sublists
+
+
+def cleaner(data: pd.DataFrame):
     """formats extracted data:
 #step1: delete rows with Q!=47
 #step2: round up angles
@@ -82,24 +87,26 @@ def cleaner(data:pd.DataFrame):
         _pd.Dataframe: desired dataframe
     """
 
-    HQ_data=data.drop(data[data.Q!=47].index)
-    indexes=HQ_data['theta'].apply(np.floor)
-    if len(indexes)==len(HQ_data):
-        HQ_data=HQ_data.set_index(indexes).drop('theta',axis=1)
-    #need to find a better alternative to round (theta)
-    HQ_data=HQ_data[~HQ_data.index.duplicated(keep='first')]
+    HQ_data = data.drop(data[data.Q != 47].index)
+    indexes = HQ_data['theta'].apply(np.floor)
+    if len(indexes) == len(HQ_data):
+        HQ_data = HQ_data.set_index(indexes).drop('theta', axis=1)
+    # need to find a better alternative to round (theta)
+    HQ_data = HQ_data[~HQ_data.index.duplicated(keep='first')]
     return HQ_data
 
-def spinPackager(spins,diplacement):
+
+def spinPackager(spins, diplacement):
     """packages a set of 0-360 data to the unit displacement
 
     Args:
         spins (pd.DataFrame[]): a list of dataframes of 0-360 data
         diplacement (int): the indexed displacement of robot
-        
+
     Returns: Full data for a given displacement (unit)
     """
     return
+
 
 def runPackager(displacement, run):
     """_summary_: standadizes the number of angles for a given displacement
@@ -109,34 +116,31 @@ def runPackager(displacement, run):
         run (_type_): _description_
     """
     return
-    
-
 
 
 def DataInterface():
     """_summary_: _description_
         Extractor() extracts data from a .txt file; returns an array of strings (each line an element)
-        
+
         SectionAror() takes in an array of strings and returns an array of arrays od strings (separated by Scan #)
-        
+
         ConvertDataToPd() takes in an array of arrays of strings and returns an array of  dataframes
-        
+
         cleaner() takes in a dataframe; cleans it up and drop an array of 0-360 dataframe
-        
+
     """
-    
-    temp_file:list[dict]=[]
-    scans:list[pd.DataFrame]=[]
-    rawdata=extractor("data/master_data.txt")
-    sectionedData=sectionAtor(rawdata)
-    print('NUmber of data in 1 scan',len(sectionedData[0]))
+
+    temp_file: list[dict] = []
+    scans: list[pd.DataFrame] = []
+    rawdata = extractor("data/master_data.txt")
+    sectionedData = sectionAtor(rawdata)
+    print('NUmber of data in 1 scan', len(sectionedData[0]))
     for element in sectionedData:
-        temp_file=cleaner(convertDatatoPd(strings2dict(element)))
+        temp_file = cleaner(convertDatatoPd(strings2dict(element)))
         scans.append(temp_file)
     return scans
-        
-        
-        
+
+
 """bugs: after rounding up angles, the dataframe is not indexed properly sometimes, it does not go from 0-360; floor doesnt solve this issue
 
 Egde cases:
@@ -149,14 +153,17 @@ Egde cases:
 
 Clarifications (assumptions):
 +the comparation function would be called directly from driving API; called everytime a new scan is received
-+the new scan will then ber cleaned up and packaged into a dataframe
-+The dataframe will then be compared to re corresponding dataframe in master data; how to do this?
++the new scan will then be cleaned up and packaged into a dataframe
++The dataframe will then be compared to corresponding dataframe in master data; how to do this?
     ==> drive function which calls the comparator will also send an index sequencially incremented when the function is called
     ==> this index ought to exist as a global variable in the file from which the comparator is called
+    
+Performance concerns:
++rewrite work in cython
 """
 
 
-master_data=DataInterface()
+master_data = DataInterface()
 
 for elemet in master_data:
     print(len(elemet))
@@ -164,5 +171,4 @@ for elemet in master_data:
 
 print(len(DataInterface()[2]))
 
-#print(master_data.index)
-
+# print(master_data.index)
--- dataProcessor.py
+++ dataProcessor.py
@@ -3,20 +3,30 @@ from hashlib import new
 from typing import overload
 from DataInterface import extractor, convertDatatoPd, sectionAtor, strings2dict, DataInterface
 import pandas as pd
-
+import random
+"""
 class InadquateData(error):
     pass
+"""
 
-def preComparator(data:list[pd.DataFrame]):
+
+def compa_adjust(newScan:pd.DataFrame, masterIndex:int, masterData:list[pd.DataFrame]):
     """Compares the newly taken scan to master data and make adjustments to make the comparision right
-
+    
     Args:
-        data (list[pd.DataFrame]): Oncoming  
+        data (list[pd.DataFrame]): Oncoming  scan
+        
+    returns: index object of the comon indexes
     """
-    return
+    relevant_master_data=masterData[masterIndex]
+    #get index comon to both dataframes
+    master_index=relevant_master_data.index
+    newScan_index=newScan.index
+    comonIndexes=master_index.intersection(newScan_index)
+    return comonIndexes
 
-def comparator(data:list[pd.DataFrame]):
-    """_summary_: takes in newly taken scan and compares it to master data
+def comparator(newScanData:list[pd.DataFrame],comonIndexes:pd.Index, masterIndex:int):
+    """_summary_: takes in newly taken scan and compares it to adequate element in master data
 
     Args:
         data (list[pd.DataFrame]): newly taken scan
@@ -27,16 +37,18 @@ def comparator(data:list[pd.DataFrame]):
     Returns:
         _type_: _description_
     """
-    masterData=DataInterface()
+    masterData=DataInterface()[masterIndex]
+    matching_masterData=masterData.loc[comonIndexes]
+    matching_newScanData=newScanData.loc[comonIndexes]
     differences:list[float]=[]
-    if len(masterData)==data:
-        for index,item in enumerate(data):
-            percentageDiff=(item['Dist']-masterData['Dist'])/masterData['Dist']
-            differences.append(percentageDiff)
+    #we're now comparing a dataframe to another dataframe
+    if len(matching_masterData)==len(matching_newScanData):
+        percentageDiff=abs(((matching_masterData['Dist']-matching_newScanData['Dist'])/matching_masterData['Dist'])*100)
     else:
-        raise InadquateData("The data is not adequate")
+        #raise InadquateData("The data is not adequate")
+        print("The data is not adequate")
         
-    return differences
+    return percentageDiff
 
 def thresholder(data:list[pd.DataFrame]):
     change:bool=False
@@ -47,5 +59,7 @@ def thresholder(data:list[pd.DataFrame]):
     return change
     
 
+test_index=[1,2,3,4]
+#test_index=test_index.sort()
+print(comparator(DataInterface()[2],test_index,8))
 
-    
