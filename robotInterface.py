9cc312c4f63c5745f95a43c908d7b7dee4b17653
Herimino Rakotondrafara <heriminorakotondrafara@Heriminos-MacBook-Pro.local>
2022-08-10 12:08:22 -0400

    final version

--- robotInterface.py
+++ robotInterface.py
@@ -19,12 +19,12 @@ class robotInterface:
                 takes in incomming data and index of master data to compare it with; returns True if the data is different
     """
     def __init__(self,addy):
-        self.masterData=addy
+        self.__masterData:list[str]=addy
         
     def getmasterData(self):
         temp_file = []
         scans = []
-        rawdata = extractor(self.masterData)
+        rawdata = extractor(self.__masterData)
         sectionedData = sectionAtor(rawdata)
         #print('NUmber of data in 1 scan', len(sectionedData[0]))
         for element in sectionedData:
@@ -34,7 +34,8 @@ class robotInterface:
         
         
     def compare(self,incomindData,masterIndex,masterData):
-        """summary: takes in incomming data and index of master data to compare it with; returns True if the data is different
+        """summary: takes in incomming data and index of master data to compare it with;
+        returns True if the data is different
         
         [Note]:to avoid multiple calls to getmasterData(), intantialize getmasterData() and then feed it to this method
         """
@@ -45,3 +46,4 @@ class robotInterface:
         return isDifferent
     
 
+robotobj=robotInterface('/home/pi/Desktop/masterData.txt')
